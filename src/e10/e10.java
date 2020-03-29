20dbe123299f335000990b212d4d3c322df565f7
jrugar74 <49482149+juanrugar@users.noreply.github.com>
2020-03-29 21:11:08 +0200

    e10 Part 2 added
    Odd number sorting and total sum added

--- src/e10/e10.java
+++ src/e10/e10.java
@@ -13,11 +13,12 @@ public class e10 {
 		
 		//local variables for limit value
 		int userNum = 0;
+		int evenTotal = 0;
 		Scanner scan = new Scanner(System.in);
 		boolean correctType = false;
 	    boolean correctValue = false;
 		//data entry
-		System.out.println("Please, enter a integer number withim limits (20-100): ");
+		System.out.println("Please, enter a integer number within limits (20-100): ");
 		correctType = scan.hasNextInt();
 		//data type validation
 		if (!correctType) {
@@ -32,6 +33,19 @@ public class e10 {
 			
 			System.out.println(userNum+ " is within limits.");
 		}
+		
+		//Part 2 - even number sorting and total sum
+		
+		for (int i = 0; i<= userNum; i++) {
+			
+			if (!(i % 2 == 0)) {
+				System.out.println(i);
+				evenTotal = evenTotal + i;
+			}
+		}
+		
+		System.out.println("Total sum of even numbers up to "+userNum+" is: "+evenTotal);
+		
 		
 	}
 
